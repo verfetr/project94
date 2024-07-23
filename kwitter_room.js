80a7c928bace36cb9ee5fb2c24910b3d87124b68
verfetr <100525400+verfetr@users.noreply.github.com>
2024-07-23 16:07:21 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBsVpofbvi4nSAdIcnidjPFmxl5bx6EpkU",
+    authDomain: "kwitter-9014d.firebaseapp.com",
+    databaseURL: "https://kwitter-9014d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-9014d",
+    storageBucket: "kwitter-9014d.appspot.com",
+    messagingSenderId: "118667127286",
+    appId: "1:118667127286:web:f32b95a84136c75cf0bfd7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
